489957ab62252a69f805e6124acdd2dd5b2b624c
soujanya-rai <68293437+soujanya-rai@users.noreply.github.com>
2021-05-20 16:34:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,23 +2,23 @@ var fixedRect, movingRect;
 
 function setup() {
   createCanvas(1200,800);
+  
   fixedRect = createSprite(600, 400, 50, 80);
-  fixedRect.shapeColor = "green";
-
   movingRect = createSprite(400,200,80,30);
-  movingRect.shapeColor = "green";
-
   rect1 = createSprite(300, 200, 50, 60);
-  rect1.shapeColor = "yellow";
-
   rect2 = createSprite(500, 200, 50, 60);
-  rect2.shapeColor = "yellow";
+  
 }
 
 function draw() {
   background(0,0,0);  
   movingRect.x = World.mouseX;
   movingRect.y = World.mouseY;
+  
+  fixedRect.shapeColor = "green";
+  movingRect.shapeColor = "green";
+  rect1.shapeColor = "yellow";
+  rect2.shapeColor = "yellow";
 
   if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
       && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
